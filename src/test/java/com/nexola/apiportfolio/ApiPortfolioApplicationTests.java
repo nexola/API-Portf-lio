abbb9f091ec4e03611790fe5577fb7e3a60097e5
nexola <vitormatheusfv@gmail.com>
2024-02-09 15:45:02 -0300

    Projeto Spring criado

---
+++ src/test/java/com/nexola/apiportfolio/ApiPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexola.apiportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
