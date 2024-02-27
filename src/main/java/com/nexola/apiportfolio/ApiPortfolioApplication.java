07a3c508465efd38e05cd31f6a6666db1b52d9e7
nexola <vitormatheusfv@gmail.com>
2024-02-27 15:41:41 -0300

    Deploy render

--- src/main/java/com/nexola/apiportfolio/ApiPortfolioApplication.java
+++ src/main/java/com/nexola/apiportfolio/ApiPortfolioApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiPortfolioApplication {
 
-
 	public static void main(String[] args) {
+		System.setProperty("https.protocols", "TLSv1");
 		SpringApplication.run(ApiPortfolioApplication.class, args);
 	}
 
